46f03e9bec19bd1b40970cf2e0c511f2fc86d656
Adrian Giełzak <adrian.gielzak@outlook.com>
2018-06-09 14:20:36 +0200

    Initialize new project - CRM System

---
+++ src/main/java/com/example/crm_system/CrmSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.crm_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmSystemApplication.class, args);
+	}
+}
