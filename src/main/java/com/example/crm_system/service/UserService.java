db917da718cea75ab38c2ba9264407fee706de43
Krzysztof Milczarek <kjmilczarek@gmail.com>
2018-06-10 10:20:11 +0200

    Add UserRepository and UserService

---
+++ src/main/java/com/example/crm_system/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.crm_system.service;
+
+import com.example.crm_system.model.User;
+import com.example.crm_system.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+
+}
