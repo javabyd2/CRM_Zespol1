5f011765eb5446dd45399073c4c7a838dc3d6bb2
Adrian Giełzak <adrian.gielzak@outlook.com>
2018-06-10 09:50:17 +0200

    adding user service and user repository

---
+++ src/main/java/com/example/crm_system/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.crm_system.repository;
+
+import com.example.crm_system.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
