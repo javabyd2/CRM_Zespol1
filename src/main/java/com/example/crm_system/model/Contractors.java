2e884002fe1dbabe22828634d6bb9f60f7ece435
Krzysztof Milczarek <kjmilczarek@gmail.com>
2018-06-10 09:52:03 +0200

    Add @Entity, @Getter, @Setter in User and Contractor, add Id in User and Contractor

--- src/main/java/com/example/crm_system/model/Contractors.java
+++ src/main/java/com/example/crm_system/model/Contractors.java
@@ -4,15 +4,28 @@ package com.example.crm_system.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Data
 @Builder
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Contractors {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String name;
     private String industry;
