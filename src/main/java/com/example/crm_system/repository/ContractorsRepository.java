afc0dbb71fee92976b0d29dcba9f27fffc1c05f7
Natalia Palimaka <npalimaka@gmail.com>
2018-06-10 15:20:15 +0200

    Contractors repository and service

---
+++ src/main/java/com/example/crm_system/repository/ContractorsRepository.java
@@ -0,0 +1,7 @@
+package com.example.crm_system.repository;
+
+import com.example.crm_system.model.Contractors;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContractorsRepository extends JpaRepository<Contractors, Long> {
+}
---
+++ src/main/java/com/example/crm_system/service/ContractorsService.java
@@ -0,0 +1,21 @@
+package com.example.crm_system.service;
+
+import com.example.crm_system.model.Contractors;
+import com.example.crm_system.repository.ContractorsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContractorsService {
+
+    private ContractorsRepository contractorsRepository;
+
+    @Autowired
+    public ContractorsService(ContractorsRepository contractorsRepository) {
+        this.contractorsRepository = contractorsRepository;
+    }
+
+    public Contractors addContractor(Contractors contractors){
+        return contractorsRepository.save(contractors);
+    }
+}
