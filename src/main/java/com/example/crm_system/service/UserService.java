4c0753035a529508d68536ae58c7c1f7d11c1868
Adrian Giełzak <adrian.gielzak@outlook.com>
2018-06-12 22:57:24 +0200

    update UserService

--- src/main/java/com/example/crm_system/service/UserService.java
+++ src/main/java/com/example/crm_system/service/UserService.java
@@ -8,58 +8,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
-<<<<<<< HEAD
-
-    private static final String DEFAULT_ROLE = "ROLE_USER";
-    private UserRepository userRepository;
-    private UserRoleRepository roleRepository;
-
-    @Autowired
-    public UserService(UserRepository userRepository, UserRoleRepository roleRepository) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-    }
-
-    @Transactional
-    public User addWithDefaultRole(User user) {
-        UserRole defaultRole = roleRepository.findByRole(DEFAULT_ROLE);
-        user.getRoles().add(defaultRole);
-        userRepository.save(user);
-        return user;
-    }
-
-    public void delete(User user){
-        userRepository.delete(user);
-    }
-}
-=======
 public interface UserService {
     public User findUserByEmail(String email);
     public void saveUser(User user);
 }
-
-
-//@Service("userService")
-//public class UserService {
+//<<<<<<< HEAD
 //
-//    @Autowired
 //    private static final String DEFAULT_ROLE = "ROLE_USER";
-//    @Autowired
 //    private UserRepository userRepository;
-//    @Autowired
-//    private RoleRepository roleRepository;
-//
-//    //@Autowired
-//    //private BCryptPasswordEncoder bCryptPasswordEncłoder;
-//
+//    private UserRoleRepository roleRepository;
 //
 //    @Autowired
-//    public void setUserRepository(UserRepository userRepository) {
+//    public UserService(UserRepository userRepository, UserRoleRepository roleRepository) {
 //        this.userRepository = userRepository;
-//    }
-//
-//    @Autowired
-//    public void setRoleRepository(RoleRepository roleRepository) {
 //        this.roleRepository = roleRepository;
 //    }
 //
@@ -71,12 +32,55 @@ public interface UserService {
 //        return user;
 //    }
 //
-//
-//
-//
-//
-//
-//
-//
+//    public void delete(User user){
+//        userRepository.delete(user);
+//    }
 //}
->>>>>>> adrian_reviewLogin
+//=======
+//public interface UserService {
+//    public User findUserByEmail(String email);
+//    public void saveUser(User user);
+//}
+//
+//
+////@Service("userService")
+////public class UserService {
+////
+////    @Autowired
+////    private static final String DEFAULT_ROLE = "ROLE_USER";
+////    @Autowired
+////    private UserRepository userRepository;
+////    @Autowired
+////    private RoleRepository roleRepository;
+////
+////    //@Autowired
+////    //private BCryptPasswordEncoder bCryptPasswordEncłoder;
+////
+////
+////    @Autowired
+////    public void setUserRepository(UserRepository userRepository) {
+////        this.userRepository = userRepository;
+////    }
+////
+////    @Autowired
+////    public void setRoleRepository(RoleRepository roleRepository) {
+////        this.roleRepository = roleRepository;
+////    }
+////
+////    @Transactional
+////    public User addWithDefaultRole(User user) {
+////        UserRole defaultRole = roleRepository.findByRole(DEFAULT_ROLE);
+////        user.getRoles().add(defaultRole);
+////        userRepository.save(user);
+////        return user;
+////    }
+////
+////
+////
+////
+////
+////
+////
+////
+////}
+//>>>>>>> adrian_reviewLogin
