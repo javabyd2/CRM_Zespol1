f1fe2801a8a696985f213a10e558d29736e37e33
Natalia Palimaka <npalimaka@gmail.com>
2018-06-15 22:38:48 +0200

    temporarly commented out

--- src/main/java/com/example/crm_system/repository/ContactsRepository.java
+++ src/main/java/com/example/crm_system/repository/ContactsRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface ContactsRepository extends JpaRepository<Contacts, Long> {
 
     //change values depending on business logic - ask Krzys if needed
-    @Modifying
-    @Query("update Contacts c set c.phone = ?1, c.email = ?2, c.assignedTo = ?3 where c.id = ?4")
-    public void updateContact(Long id, Long phone, String email, String assignedTo);
+//    @Modifying
+//    @Query("update Contacts c set c.phone = ?1, c.email = ?2, c.assignedTo = ?3 where c.id = ?4")
+//    public void updateContact(Long id, Long phone, String email, String assignedTo);
 }
