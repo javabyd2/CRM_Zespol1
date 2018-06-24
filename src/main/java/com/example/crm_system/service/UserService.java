212165acff28c509befdbc8e624f885aabe4f818
Natalia Palimaka <npalimaka@gmail.com>
2018-06-24 20:24:41 +0200

    findByEmail and saveUser service tested

--- src/main/java/com/example/crm_system/service/UserService.java
+++ src/main/java/com/example/crm_system/service/UserService.java
@@ -2,7 +2,19 @@ package com.example.crm_system.service;
 
 import com.example.crm_system.model.User;
 
+import java.util.List;
+
 public interface UserService {
-    public User findUserByEmail(String email);
-    public void saveUser(User user);
+
+    User findUserByEmail(String email);
+
+    void saveUser(User user);
+
+    List<User> getUsers();
+
+    void deleteUser(Long id);
+
+    User getUserById(Long id);
+
+    void editUser(Long id, User user);
 }
