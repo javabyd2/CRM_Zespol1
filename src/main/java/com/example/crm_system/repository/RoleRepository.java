281467ccc1491934fd0a7aa3e5d8c1ae5c8319d5
Natalia Palimaka <npalimaka@gmail.com>
2018-06-27 16:59:19 +0200

    fixed annotations

--- src/main/java/com/example/crm_system/repository/RoleRepository.java
+++ src/main/java/com/example/crm_system/repository/RoleRepository.java
@@ -3,9 +3,6 @@ package com.example.crm_system.repository;
 import com.example.crm_system.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.stereotype.Repository;
-
-@Repository("roleRepository")
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     Role findByRole(String role);
 
