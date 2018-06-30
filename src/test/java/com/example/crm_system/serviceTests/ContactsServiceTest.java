6e0066a654d0cdfd9933a7d8a258717f6b12fd1c
Natalia Palimaka <npalimaka@gmail.com>
2018-06-30 10:21:01 +0200

    simple test class

--- src/test/java/com/example/crm_system/serviceTests/ContactsServiceTest.java
+++ src/test/java/com/example/crm_system/serviceTests/ContactsServiceTest.java
@@ -9,28 +9,41 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 public class ContactsServiceTest {
 
     private ContactsRepository contactsRepository;
     private ContactsService contactsService;
+    private Contacts contacts;
 
     @Before
     public void setUp(){
         contactsRepository = Mockito.mock(ContactsRepository.class);
         contactsService = new ContactsService(contactsRepository);
-    }
-
-    @Test
-    public void whenAddedContactCanBeFind(){
-        Contacts contacts = new Contacts();
+        contacts = new Contacts();
         String email = "newContact@email.cs";
         contacts.setMail(email);
         contacts.setId(1L);
+    }
+
+    @Test
+    public void whenSavedDateCreatedIsSet(){
+        contactsService.saveContact(contacts);
+        assertThat(contacts.getDataCreated()).isNotNull();
+
+        contactsService.deleteContact(1L);
+        Contacts found = contactsService.getContactById(contacts.getId()).orElse(null);
+        assertThat(found).isNull();
+    }
+
+    @Test
+    public void whenDeletedRecordNotFound(){
         contactsService.saveContact(contacts);
         contactsService.deleteContact(1L);
-//        assertThat(contacts.getId()).isNull();
-//        assertThat(contacts.getMail()).isNull();
-//        assertThat(contacts.getMail()).isEqualTo(newDetails.getMail());
+
+        Contacts found = contactsService.getContactById(contacts.getId()).orElse(null);
+        assertThat(found).isNull();
     }
 }
